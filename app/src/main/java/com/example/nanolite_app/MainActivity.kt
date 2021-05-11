0105e50e53692b2042cf8b48867068e346125bf8
faiqdhimas <faiq.dhms@gmail.com>
2021-05-11 13:10:08 +0700

    initial commit

---
+++ app/src/main/java/com/example/nanolite_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nanolite_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//NanoLite - App
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
